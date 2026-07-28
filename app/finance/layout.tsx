a1b6cbe381e5ed0763ad1f4a6df55518cbb50f39
Godofredo Quispe Tipo <u202120772@upc.edu.pe>
2026-07-28 04:42:26 -0500

    feat: updade structure file

--- app/finance/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardLayout } from "@/components/layout/DashboardLayout";
-
-export default function FinanzasLayout({ children }: { children: React.ReactNode }) {
-  return <DashboardLayout title="Finanzas">{children}</DashboardLayout>;
-}
